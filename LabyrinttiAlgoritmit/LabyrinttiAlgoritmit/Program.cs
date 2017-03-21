3f9e4b482a9a31f6ff2bc6fbd27586e8b652585b
K1625 <K1625@PROJ-422-WS-02>
2017-03-21 14:31:13 +0200

    Labyrintti algoritmit alussa

---
+++ LabyrinttiAlgoritmit/LabyrinttiAlgoritmit/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabyrinttiAlgoritmit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrix = new int[4, 4] { { 1, 0, 0, 0 },
+                                            {1, 1, 0, 1},
+                                            {0, 1, 0, 0},
+                                            {1, 1, 1, 1}
+            };
+            Maze maze = new Maze(matrix);
+        }
+    }
+}
